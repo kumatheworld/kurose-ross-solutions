5777ea2c732aa580c52a7f9a693fa224f737162c
kumatheworld <kum4th3w0rld@gmail.com>
2024-08-08 19:15:21 +0100

    Add c2a1_client.py

---
+++ extra/c2a1_client.py
@@ -0,0 +1,18 @@
+import socket
+
+import typer
+
+
+def main(
+    server_host: str, server_port: int, filename: str, bufsize: int = 1024
+) -> None:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((server_host, server_port))
+        request = f"GET /{filename} HTTP/1.0\r\n\r\n"
+        client_socket.send(request.encode())
+        while response := client_socket.recv(bufsize).decode():
+            print(response, end="")
+
+
+if __name__ == "__main__":
+    typer.run(main)
