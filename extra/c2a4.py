4697042b676bc604e15e1ce3cdea10401d420b53
kumatheworld <kum4th3w0rld@gmail.com>
2024-08-29 21:49:55 +0100

    Improve structure

--- extra/c2a4.py
+++ extra/c2a4.py
@@ -15,45 +15,31 @@ def main(server_ip: str = "", bufsize: int = 1024) -> None:
                 message = tcp_cli_sock.recv(bufsize).decode()
                 print(message)
                 fileuri = message.split()[1]
-                _, _, filename = fileuri.partition("://")
-                print(f"{filename=}")
+                _, _, host_n_file = fileuri.partition("://")
+                print(f"{host_n_file=}")
                 try:
                     # Check wether the file exist in the cache
-                    f = open(filename)
+                    f = open(host_n_file)
                 # Error handling for file not found in cache
                 except IOError:
-                    # Create a socket on the proxyserver
-                    c = ...  # Fill in start. # Fill in end.
-                    hostn = filename.replace("www.", "", 1)
-                    print(hostn)
-                    # Connect to the socket to port 80
-                    # Fill in start.
-                    # Fill in end.
-                    # Create a temporary file on this socket and ask port 80 for the file requested by the client
-                    fileobj = c.makefile("r", 0)
-                    fileobj.write(f"GET http://{filename} HTTP/1.0\n\n")
-                    # Read the response into buffer
-                    # Fill in start.
-                    # Fill in end.
-                    # Create a new file in the cache for the requested file.
-                    # Also send the response in the buffer to client socket and the corresponding file in the cache
-                    tmpFile = open("./" + filename, "wb")
-                    # Fill in start.
-                    # Fill in end.
-                    # HTTP response message for file not found if file is not found.
-                    # Fill in start.
-                    # Fill in end.
+                    # Create a socket on the proxyserver and connect to the server port 80
+                    server_host, _, filename = host_n_file.partition("/")
+                    with socket.create_connection((server_host, 80)) as client_socket:
+                        # Request the file
+                        request = f"GET /{filename} HTTP/1.0\r\n\r\n"
+                        client_socket.send(request.encode())
+                        # If the file exists, cache it and send it to the client.
+                        # If it doesn't, return an error message.
+                        # The following is not correct and should be fixed.
+                        while response := client_socket.recv(bufsize).decode():
+                            print(response, end="")
                 else:
+                    print("Read from cache")
                     outputdata = f.readlines()
                     f.close()
-                    # ProxyServer finds a cache hit and generates a response message
-                    tcp_cli_sock.send("HTTP/1.0 200 OK\r\n")
-                    tcp_cli_sock.send("Content-Type:text/html\r\n")
+                    # Send the file to the client
                     # Fill in start.
                     # Fill in end.
-                    print("Read from cache")
-            # Fill in start.
-            # Fill in end.
 
 
 if __name__ == "__main__":
