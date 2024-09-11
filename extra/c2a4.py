dd8007bfbd489eb60538c964f4a133616aefde57
kumatheworld <kum4th3w0rld@gmail.com>
2024-09-11 22:20:48 +0100

    Split response by whitespaces

--- extra/c2a4.py
+++ extra/c2a4.py
@@ -87,7 +87,7 @@ def main(server_ip: str = "", bufsize: int = 1024) -> None:
                     ) as proxy_client_socket:
                         proxy_client_socket.send(request.encode())
                         response = proxy_client_socket.recv(bufsize)
-                        _, status_code, _ = response.split(b"\r\n", maxsplit=2)
+                        _, status_code, _ = response.split(maxsplit=2)
                         if status_code == "304":
                             connection_socket.send("HTTP/1.0 200 OK\r\n\r\n".encode())
                             for line in file:
