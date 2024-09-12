5c6b400f821b2f2933f5febde90de2bfb267a6a2
kumatheworld <kum4th3w0rld@gmail.com>
2024-09-12 22:34:39 +0100

    Call decode() later

--- extra/c2a1_client.py
+++ extra/c2a1_client.py
@@ -9,8 +9,8 @@ def main(
     with socket.create_connection((server_host, server_port)) as client_socket:
         request = f"GET /{filename} HTTP/1.0\r\n\r\n"
         client_socket.send(request.encode())
-        while response := client_socket.recv(bufsize).decode():
-            print(response, end="")
+        while response := client_socket.recv(bufsize):
+            print(response.decode(), end="")
 
 
 if __name__ == "__main__":
